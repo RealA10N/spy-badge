1eed72d958e0455cdbe1041477b8148047f8074b
Alon (PC) <downtown2u@gmail.com>
2021-08-19 20:08:22 +0300

    ✨ Added `generate_image` fucnction

---
+++ img.py
@@ -0,0 +1,23 @@
+import os
+import ipinfo
+from PIL import Image, ImageDraw, ImageFont
+
+handler = ipinfo.getHandler(os.environ['IPINFO_TOKEN'])
+font = ImageFont.load_default()
+
+
+def generate_image(ip: str):
+    details = handler.getDetails(ip)
+
+    text = ' | '.join((
+        f'{name}: {value}'
+        for name, value in details.details.items()
+    ))
+
+    size = font.getsize(text)
+    img = Image.new('L', size, color='black')
+
+    draw = ImageDraw.Draw(img)
+    draw.text((0, 0), text, fill='white', font=font)
+
+    return img
